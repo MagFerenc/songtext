6d3f4503612df8604f1e27e6da38a44ffdc1a80e
MagFerenc <F.Mag@bst-regeltechnik.de>
2026-08-26 19:31:04 +0300

    songs.js

--- songs.js
+++ songs.js
@@ -22,7 +22,7 @@
 const SONGS = [
   { id: 1, title: "Örökké tart – Példa 1", type: "pdf", url: "dalok/Orokke tart.pdf" },
   { id: 2, title: "Példa dal x – Teszt 2", type: "pdf", url: "dalok/teszt-dal.pdf" },
-  { id: 3, title: "Példa dal 3 – Öröm", type: "web", url: "https://www.dalszoveg.hu/d1179796/zambo_jimmy/engem_csak_szeretni_kell-dalszoveg.html" },
+  { id: 3, title: "Példa dal 3 – Öröm", type: "pdf", url: "dalok/Orokke tart.pdf" },
   { id: 4, title: "Példa dal 4 – Teszt 4", type: "pdf", url: "dalok/teszt-dal.pdf" },
   { id: 5, title: "Szerelem első vérig", type: "web", url: "dalok/02-szerelem-elso-verig.html" },
 ];
