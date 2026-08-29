3786649d056fccc66ad320fa0723cd2c259c6000
MagFerenc <F.Mag@bst-regeltechnik.de>
2026-08-29 16:39:01 +0200

    Update songs2.js

--- songs.js
+++ songs.js
@@ -22,5 +22,6 @@
 const SONGS = [
   { id: 1, title: "Mindig kell egy barat", type: "pdf", url: "Dalok/Mindig-kell-egy-barat.pdf" },
   { id: 2, title: "Mi kéne még", type: "pdf", url: "Dalok/Mi-kene-meg.pdf" },
-  { id: 3, title: "Ujra itt van plusz", type: "pdf", url: "Dalok/Ujra-itt-van-plusz.pdf" },
+  { id: 3, title: "Seemann", type: "pdf", url: "Dalok/Seemann.pdf" },
+  { id: 4, title: "Ujra itt van plusz", type: "pdf", url: "Dalok/Ujra-itt-van-plusz.pdf" },
  ];
