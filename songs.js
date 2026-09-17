92acf2f5a3c22d07723dbce1fd6ce5ec404f0a2e
MagFerenc <F.Mag@bst-regeltechnik.de>
2026-09-18 00:09:25 +0200

    Update songs.js

--- songs.js
+++ songs.js
@@ -31,7 +31,7 @@ const SONGS = [
   { id: 202, title: "Mindig kell egy barát",                         type: "pdf", url: "Dalok/200-Pop/202-Mindig-kell-egy-barat.pdf" },
   { id: 203, title: "Szép nyári nap",                                type: "pdf", url: "Dalok/200-Pop/203-Szep-nyari-nap.pdf" },
   { id: 401, title: "Pedro kocsmája",                                type: "pdf", url: "Dalok/400-mulatos/401-Pedro-kocsmaja.pdf" },
-  { id: 402, title: "Szép nyári nap",                                type: "pdf", url: "Dalok/400-mulatos/203-Szep-nyari-nap.pdf" },
+  { id: 402, title: "Love In Portofino",                             type: "pdf", url: "Dalok/400-mulatos/402-Love-In-Portofino.pdf" },
   { id: 801, title: "Örökké tart",                                   type: "pdf", url: "Dalok/pittnerf/Szövegek_összefuzött.pdf", pageFrom: 1, pageTo: 2 },
   { id: 802, title: "Szerelem első vérig",                           type: "pdf", url: "Dalok/pittnerf/Szövegek_összefuzött.pdf", pageFrom: 3 },
   { id: 803, title: "Utcabál - Hungária",                            type: "pdf", url: "Dalok/pittnerf/Szövegek_összefuzött.pdf", pageFrom: 4 },
